243d9c80d117f4e277856d9e485064146414ca2e
ebolatumor <komlosi.attila@gmail.com>
2017-04-01 17:40:27 +0200

    initial commit

---
+++ AutoSuggest/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <AutoSuggest></AutoSuggest>
+  </div>
+</template>
+
+<script>
+import AutoSuggest from './components/AutoSuggest'
+
+export default {
+  name: 'app',
+  components: {
+    AutoSuggest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
